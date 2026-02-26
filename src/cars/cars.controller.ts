c3acba914d51fd718c3d8c181a51704dea52fca8
Jose Angel Hernandez Radilla <jose.angel.hdz.rda@gmail.com>
2026-02-26 15:17:13 -0600

    pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -8,8 +8,11 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dtos/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -26,8 +29,8 @@ export class CarsController {
   }
 
   @Post()
-  createCar(@Body() body: any) {
-    return body;
+  createCar(@Body() createCardDto: CreateCarDto) {
+    return createCardDto;
   }
 
   @Patch(':id')
